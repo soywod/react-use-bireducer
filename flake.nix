14bb79045dc0bebb90b5430b8fa3b5c39441ddac
Clément DOUIN <clement.douin@posteo.net>
2022-05-24 20:33:32 +0200

    add tsdx

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "React hook for managing reducers and effects together.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Common tools
+              coreutils
+              ripgrep
+
+              # Nix LSP + formatter
+              rnix-lsp
+              nixpkgs-fmt
+
+              # Node.js env
+              nodejs-16_x
+              yarn
+            ];
+            shellHook = ''
+              # add node_modules/.bin to path
+              export PATH="$PWD/node_modules/.bin/:$PATH"
+            '';
+          };
+        }
+      );
+}
