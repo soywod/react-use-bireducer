4a69fd8b859e493d19e4edf35a5d43ec70c1205e
Clément DOUIN <clement.douin@posteo.net>
2022-05-26 10:02:51 +0200

    add useBireducer hook + tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,43 @@
-import * as React from 'react';
+import { useCallback, useEffect, useReducer, useRef, useState } from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+export type StateReducer<S, A, E> = (state: S, action: A) => [S, E[]];
+
+export type EffectReducer<E> = (effect: E) => EffectCleanup | void;
+export type EffectCleanup = () => void;
+
+export function useBireducer<S, A, E>(
+  stateReducer: StateReducer<S, A, E>,
+  effectReducer: EffectReducer<E>,
+  defaultState: S
+) {
+  const [effects, setEffects] = useState<E[]>([]);
+  const cleanups = useRef<EffectCleanup[]>([]);
+
+  const reducer = useCallback(
+    (state: S, action: A): S => {
+      const [nextState, nextEffects] = stateReducer(state, action);
+      setEffects(effects => [...nextEffects.reverse(), ...effects]);
+      return nextState;
+    },
+    [stateReducer]
+  );
+
+  useEffect(() => {
+    const effect = effects.pop();
+    if (effect) {
+      const cleanup = effectReducer(effect);
+      if (cleanup) cleanups.current.push(cleanup);
+      setEffects([...effects]);
+    }
+  }, [effects, effectReducer]);
+
+  useEffect(() => {
+    return () => {
+      cleanups.current.forEach(cleanup => {
+        cleanup();
+      });
+    };
+  }, []);
+
+  return useReducer(reducer, defaultState);
+}
